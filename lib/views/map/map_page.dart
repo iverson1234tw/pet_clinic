410cdf110e77bb8d95e876ba06e6c7c6caecda73
Josh Chen <colbychen25@gmail.com>
2025-05-19 22:10:22 +0800

    [feature] 加入通話、導航功能

--- lib/views/map/map_page.dart
+++ lib/views/map/map_page.dart
@@ -4,8 +4,9 @@ import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:pet_clinic_app/views/map/map_view_model.dart';
 import 'package:provider/provider.dart';
-
+import 'package:pet_clinic_app/models/clinic_model.dart';
 import 'widgets/clinic_info_card.dart'; // 匯入你寫好的卡片元件
+import 'package:url_launcher/url_launcher.dart';
 
 /// 地圖主畫面：整合 Google Map 並由 ViewModel 提供診所座標資料與定位邏輯
 class MapPage extends StatelessWidget {
@@ -60,10 +61,12 @@ class MapPage extends StatelessWidget {
                           address: vm.selectedClinic!.address,
                           phone: vm.selectedClinic!.phone,
                           onNavigate: () {
-                            // TODO: 開啟導航功能（後續補上）
+                            // TODO: 導航功能
+                            _launchGoogleMapsNavigation(vm.selectedClinic!); // ✅ 導航
                           },
                           onCall: () {
-                            // TODO: 撥打電話功能（後續補上）
+                            // TODO: 撥打電話
+                            _launchPhoneCall(vm.selectedClinic!.phone); // ✅ 撥打電話
                           },
                         ),
                 ),
@@ -85,4 +88,27 @@ class MapPage extends StatelessWidget {
       ),
     );
   }
+
+  /// ✅ 打開 Google Maps 導航
+  void _launchGoogleMapsNavigation(Clinic clinic) async {
+    final uri = Uri.parse(
+      'https://www.google.com/maps/dir/?api=1&destination=${clinic.lat},${clinic.lng}&travelmode=driving',
+    );
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri, mode: LaunchMode.externalApplication);
+    } else {
+      debugPrint('❌ 無法開啟 Google Maps');
+    }
+  }
+
+  /// ✅ 撥打電話
+  void _launchPhoneCall(String phone) async {
+    final tel = Uri.parse('tel:$phone');
+    if (await canLaunchUrl(tel)) {
+      await launchUrl(tel);
+    } else {
+      debugPrint('❌ 無法撥打電話');
+    }
+  }
+
 }
